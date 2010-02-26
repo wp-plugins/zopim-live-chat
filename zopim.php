83053c226ba215b6b81dd422fc9589e722e823e9
bencxr <bencxr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-26 08:07:59 +0000

    some text updates
    git-svn-id: https://plugins.svn.wordpress.org/zopim-live-chat/trunk@210456 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zopim.php
+++ zopim.php
@@ -40,9 +40,12 @@ function zopimme() {
 
    // Use zopim's code...
    echo "
+<!-- Start of Zopim Live Chat Script -->
    <script type=\"text/javascript\">
 document.write(unescape(\"%3Cscript src='\" + document.location.protocol + \"//".ZOPIM_SCRIPT_DOMAIN."/?".$code."' charset='utf-8' type='text/javascript'%3E%3C/script%3E\"));
-      </script>";
+      </script>
+<!-- End of Zopim Live Chat Script -->
+";
 
    $theoptions = array();
    if (get_option('zopimLang') != "" && get_option('zopimLang') != "--") {
