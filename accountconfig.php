f3cd28d0486cfa8fddd7e008386bdbdc638ac943
bencxr <bencxr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-16 06:46:16 +0000

    Version 1.2.2
    - adds User Capability levels, allowing non-admins to use the plugin admin interface
    - Admins can use add the following code
    	$role = get_role( 'editor' ); $role->add_cap( 'access_zopim' );
    to their themes, or use the Members plugin (http://wordpress.org/extend/plugins/members/)
    to give roles the 'access_zopim' capability.
    
    
    
    
    git-svn-id: https://plugins.svn.wordpress.org/zopim-live-chat/tags/1.2.2@613072 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ accountconfig.php
@@ -0,0 +1,172 @@
+<?php
+
+// Settings page in the admin panel
+function zopim_account_config() {
+	global $usernameToCodeURL, $languagesURL, $current_user;
+
+?>
+
+<div class="wrap">
+
+<?php
+
+	if (isset($_GET["action"]) && $_GET["action"]=="deactivate") {
+		update_option('zopimSalt', "");
+		update_option('zopimCode', "zopim");
+	}
+
+	$message = "";
+	$authenticated = "";
+
+	if (isset($_POST["action"]) && $_POST["action"]=="login") {
+		if (!isset($_POST["zopimUseSSL"]) || $_POST["zopimUseSSL"] == '') {
+			$_POST["zopimUseSSL"] = "nossl";
+		}
+
+		update_option('zopimUseSSL', $_POST["zopimUseSSL"]);
+
+		if ($_POST["zopimPassword"] != "password") {
+
+			$logindata = array("email" => $_POST["zopimUsername"], "password" => $_POST["zopimPassword"]);
+			$loginresult = json_to_array(zopim_post_request(ZOPIM_LOGIN_URL, $logindata));
+
+			if (isset($loginresult->error)) {
+				$error["login"] = "<b>Could not log in to Zopim. Please check your login details. If problem persists, try connecting without SSL enabled.</b>";
+				$gotologin = 1;
+				update_option('zopimSalt', "wronglogin");
+			} else if (isset($loginresult->salt)) {
+				update_option('zopimUsername', $_POST["zopimUsername"]);
+				update_option('zopimSalt', $loginresult->salt);
+				$account = getAccountDetails(get_option('zopimSalt'));
+
+				if (isset($account)) {
+					update_option('zopimCode', $account->account_key);
+
+					if (get_option('zopimGreetings') == "") {
+						$jsongreetings = to_json($account->settings->greetings);
+						update_option('zopimGreetings', $jsongreetings);
+					}
+				}
+			} else {
+				update_option('zopimSalt', "");
+				$error["login"] = "<b>Could not log in to Zopim. We were unable to contact Zopim servers. Please check with your server administrator to ensure that <a href='http://www.php.net/manual/en/book.curl.php'>PHP Curl</a> is installed and permissions are set correctly.</b>";
+			}
+		}
+	} else if (isset($_POST["action"]) && $_POST["action"]=="signup") {
+
+		if ($_POST["zopimUseSSL"] == "") {
+			$_POST["zopimUseSSL"] = "nossl";
+		}
+		update_option('zopimUseSSL', $_POST["zopimUseSSL"]);
+
+		$createdata = array(
+			"email" => $_POST["zopimnewemail"],
+			"first_name" => $_POST["zopimfirstname"],
+			"last_name" => $_POST["zopimlastname"],
+			"display_name" => $_POST["zopimfirstname"]." ".$_POST["zopimlastname"],
+			"eref" => $_POST["zopimeref"],
+			"source" => "wordpress",
+			"recaptcha_challenge_field" => $_POST["recaptcha_challenge_field"],
+			"recaptcha_response_field" => $_POST["recaptcha_response_field"]
+		);
+
+		$signupresult = json_to_array(zopim_post_request(ZOPIM_SIGNUP_URL, $createdata));
+		if (isset($signupresult->error)) {
+			$message = "<div style='color:#c33;'>Error during activation: <b>".$signupresult->error."</b>. Please try again.</div>";
+		} else if (isset($signupresult->account_key)) {
+			$message = "<b>Thank you for signing up. Please check your mail for your password to complete the process. </b>";
+			$gotologin = 1;
+		} else {
+			$message = "<b>Could not activate account. The wordpress installation was unable to contact Zopim servers. Please check with your server administrator to ensure that <a href='http://www.php.net/manual/en/book.curl.php'>PHP Curl</a> is installed and permissions are set correctly.</b>";
+		}
+	}
+
+	$error = NULL;
+	if (get_option('zopimCode') != "" && get_option('zopimCode') != "zopim") {
+
+		$accountDetails = getAccountDetails(get_option('zopimSalt'));
+		if (!isset($accountDetails) || isset($accountDetails->error)) {
+			$gotologin = 1;
+			$error["auth"] = '
+	 <div class="metabox-holder">
+	<div class="postbox">
+		<h3 class="hndle"><span>Account no longer linked!</span></h3>
+		<div style="padding:10px;line-height:17px;">
+		We could not verify your Zopim account. Please check your password and try again.
+		</div>
+	</div>
+	 </div>'
+;
+		} else {
+			$authenticated = "ok";
+		}
+	}
+
+	if ($authenticated == "ok") {
+		if ($accountDetails->package_id=="trial") {
+			$accountDetails->package_id = "Free Lite Package + 14 Days Full-features";
+		} else {
+			$accountDetails->package_id .= " Package";
+		}
+?>
+<div id="icon-options-general" class="icon32"><br/></div><h2>Set up your Zopim Account</h2>
+<br/>
+<div style="background:#FFFEEB;padding:25px;border:1px solid #eee;">
+<span style="float:right;"><a href="admin.php?page=zopim_account_config&action=deactivate">Deactivate</a></span>
+Currently Activated Account &rarr; <b><?php echo get_option('zopimUsername'); ?></b> <div style="display:inline-block;background:#444;color:#fff;font-size:10px;text-transform:uppercase;padding:3px 8px;-moz-border-radius:5px;-webkit-border-radius:5px;"><?php echo ucwords($accountDetails->package_id); ?></div>
+<br><p><br>You can <a href="admin.php?page=zopim_customize_widget">customize</a> the chat widget, <a href="admin.php?page=zopim_instant_messaging">relay messages</a> to your favourite IM client, or <a href="admin.php?page=zopim_dashboard">launch the dashboard</a> for advanced features.
+</div>
+
+<?php } else { ?>
+<div id="icon-options-general" class="icon32"><br/></div><h2>Set up your Zopim Account</h2>
+<?php if ($error && $error["auth"]) {
+	echo $error["auth"];
+	} else if ($message == "") { ?>
+Congratulations on successfully installing the Zopim WordPress plugin! Activate an account to start using Zopim Live Chat.<br>
+<br>
+<?php } else { echo $message; } ?>
+
+<div id="existingform">
+	<div class="metabox-holder">
+		<div class="postbox">
+			<h3 class="hndle"><span>Link up to your Zopim account</span></h3>
+			<div style="padding:10px;">
+<?php if (isset($error) && isset($error["login"])) { echo $error["login"]; } ?>
+<form method="post" action="admin.php?page=zopim_account_config">
+	<input type="hidden" name="action" value="login">
+	<table class="form-table">
+
+		  <tr valign="top">
+		  <th scope="row">Zopim Username (E-mail)</th>
+		  <td><input type="text" name="zopimUsername" value="<?php echo get_option('zopimUsername'); ?>" /></td>
+		  </tr>
+
+		  <tr valign="top">
+		  <th scope="row">Zopim Password</th>
+		  <td><input type="password" name="zopimPassword" value="<?php if (get_option('zopimSalt') != "") { echo "password"; }; ?>" /></td>
+		  </tr>
+
+		  <tr valign="center">
+		  <th scope="row">Use SSL</th>
+		  <td><input type="checkbox" name="zopimUseSSL" value="zopimUseSSL" <?php if (get_option('zopimUseSSL') == "zopimUseSSL") { echo "checked='checked'"; } ?> /> uncheck this if you are unable to login</td>
+		  </tr>
+	 </table>
+		<br/>
+		The Zopim chat bar will displayed on your blog once your account is linked up.
+		<br/>
+	 <p class="submit">
+	 <input type="submit" class="button-primary" value="<?php _e('Link Up') ?>" />
+	 Don't have a zopim account? <a href="<?php echo ZOPIM_SIGNUP_REDIRECT_URL; ?>" target="_blank" data-popup="true">Sign up now</a>.
+	 </p>
+
+</form>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+</div>
+
+
+<?php } } ?>
---
+++ zopim.php
@@ -0,0 +1,209 @@
+<?php
+
+/*
+Plugin Name: Zopim Widget
+Plugin URI: http://www.zopim.com/?iref=wp_plugin
+Description: Zopim embeds a chatbar on your website, so that any visitor can chat with you directly by clicking on the chatbar.
+Author: Zopim
+Version: 1.2.2
+Author URI: http://www.zopim.com/?iref=wp_plugin
+*/
+
+define('ZOPIM_SCRIPT_DOMAIN',         "zopim.com");
+define('ZOPIM_BASE_URL',              "https://www.zopim.com/");
+define('ZOPIM_SIGNUP_REDIRECT_URL',   ZOPIM_BASE_URL."?iref=wp_plugin#signup");
+define('ZOPIM_GETACCOUNTDETAILS_URL', ZOPIM_BASE_URL."plugins/getAccountDetails");
+define('ZOPIM_SETDISPLAYNAME_URL',    ZOPIM_BASE_URL."plugins/setDisplayName");
+define('ZOPIM_IMINFO_URL',            ZOPIM_BASE_URL."plugins/getImSetupInfo");
+define('ZOPIM_IMREMOVE_URL',          ZOPIM_BASE_URL."plugins/removeImSetup");
+define('ZOPIM_LOGIN_URL',             ZOPIM_BASE_URL."plugins/login");
+define('ZOPIM_SIGNUP_URL',            ZOPIM_BASE_URL."plugins/createTrialAccount");
+define('ZOPIM_THEMES_LIST',           "http://zopim.com/assets/dashboard/themes/window/plugins-themes.txt");
+define('ZOPIM_COLORS_LIST',           "http://zopim.com/assets/dashboard/themes/window/plugins-colors.txt");
+define('ZOPIM_LANGUAGES_URL',         "http://translate.zopim.com/projects/zopim/");
+define('ZOPIM_DASHBOARD_URL',         "http://dashboard.zopim.com/?iref=wp_plugin");
+define('ZOPIM_SMALL_LOGO',            "http://zopim.com/assets/branding/zopim.com/chatman/online.png");
+define('ZOPIM_IM_LOGOS',              "http://www.zopim.com/static/images/im/");
+define('ZOPIM_THEMES_URL',            "http://");
+define('ZOPIM_COLOURS_URL',           "http://");
+
+require_once dirname( __FILE__ ) . '/accountconfig.php';
+require_once dirname( __FILE__ ) . '/customizewidget.php';
+require_once dirname( __FILE__ ) . '/imintegration.php';
+
+function add_zopim_caps() {
+	$role = get_role( 'administrator' );
+	$role->add_cap( 'access_zopim' );
+}
+
+add_action( 'admin_init', 'add_zopim_caps');
+
+
+// We need some CSS to position the paragraph
+function zopimme() {
+	global $current_user, $zopimshown;
+
+	$code = get_option('zopimCode');
+
+	if (($code == "" || $code=="zopim") && (!ereg("zopim", $_GET["page"]))&& (!ereg("zopim", $_SERVER["SERVER_NAME"]))) { return; }
+
+	// dont show this more than once
+	if (isset($zopimshown) && $zopimshown == 1) { return; }
+	$zopimshown = 1;
+
+	echo "<!--Start of Zopim Live Chat Script-->
+<script type=\"text/javascript\">
+window.\$zopim||(function(d,s){var z=\$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//cdn.zopim.com/?".$code."';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zopim Live Chat Script-->";
+
+	$theoptions = array();
+	if (get_option('zopimLang') != "" && get_option('zopimLang') != "--")
+	 $theoptions[] = " language: '".get_option('zopimLang')."'";
+
+	if (isset($current_user) && get_option("zopimGetVisitorInfo") == "checked") {
+	 $ul = $current_user->data->first_name;
+	 $useremail = $current_user->data->user_email;
+	 if ($ul!="" && $useremail != "")
+		$theoptions[] = "name: '$ul', email: '$useremail'";
+	}
+
+	echo "\n<script type=\"text/javascript\">\n\$zopim( function() {";
+
+	if (count($theoptions) > 0)
+	 echo '$zopim.livechat.set({'.implode(", ", $theoptions)."});";
+
+	get_option('zopimPosition')    != "" && print("\n\$zopim.livechat.button.setPosition('".get_option('zopimPosition')."');");
+	get_option('zopimTheme')       != "" && print("\n\$zopim.livechat.window.setTheme('".get_option('zopimTheme')."');");
+	get_option('zopimColor')       != "" && print("\n\$zopim.livechat.window.setColor('".get_option('zopimColor')."');");
+
+	if (get_option('zopimUseGreetings') == "zopimUseGreetings") {
+		if (get_option('zopimGreetings') != "") {
+			$greetings = json_to_array(get_option('zopimGreetings'));
+			foreach ($greetings as $i => $v) {
+			 foreach ($v as $j => $k) {
+				$greetings->$i->$j = str_replace("\r\n", "\\n", $greetings->$i->$j);
+				}
+			}
+			echo "\n\$zopim.livechat.setGreetings({
+'online' : ['".addslashes($greetings->online->bar)."', '".addslashes($greetings->online->window)."'],
+'offline': ['".addslashes($greetings->offline->bar)."', '".addslashes($greetings->offline->window)."'],
+'away'   : ['".addslashes($greetings->away->bar)."', '".addslashes($greetings->away->window)."']  });";
+		}
+	}
+
+	if (get_option('zopimUseBubble') == "zopimUseBubble") {
+		get_option('zopimBubbleTitle') != "" && print("\n\$zopim.livechat.bubble.setTitle('".addslashes(get_option('zopimBubbleTitle'))."');");
+		get_option('zopimBubbleText')  != "" && print("\n\$zopim.livechat.bubble.setText('".addslashes(get_option('zopimBubbleText'))."');");
+	}
+
+	if (get_option('zopimBubbleEnable') == "show")
+		echo "\n\$zopim.livechat.bubble.show(true);";
+	else if (get_option('zopimBubbleEnable') == "hide")
+		echo "\n\$zopim.livechat.bubble.hide(true);";
+
+	// this must be called last
+	if (get_option('zopimHideOnOffline') == "zopimHideOnOffline")
+	 echo "\n\$zopim.livechat.button.setHideWhenOffline(true);";
+	echo "\n})</script>";
+}
+
+function zopim_create_menu() {
+	//create new top-level menu
+	add_menu_page('Account Configuration', 'Zopim Chat', 'access_zopim', 'zopim_account_config', 'zopim_account_config', ZOPIM_SMALL_LOGO);
+
+	// add_submenu_page('zopim_about', "About", "About", "access_zopim", 'zopim_about', 'zopim_about');
+	add_submenu_page('zopim_account_config', 'Account Configuration', 'Account Setup', 'access_zopim', 'zopim_account_config', 'zopim_account_config');
+	add_submenu_page('zopim_account_config', 'Customize Widget', 'Customize', 'access_zopim', 'zopim_customize_widget', 'zopim_customize_widget');
+	add_submenu_page('zopim_account_config', 'IM Integration', 'IM Chat Bots', 'access_zopim', 'zopim_instant_messaging', 'zopim_instant_messaging');
+	add_submenu_page('zopim_account_config', 'Dashboard', 'Dashboard', 'access_zopim', 'zopim_dashboard', 'zopim_dashboard');
+
+	//call register settings function
+	add_action( 'admin_init', 'register_zopim_plugin_settings' );
+}
+
+function zopim_about() {
+	echo "about";
+}
+
+function zopim_dashboard() {
+	echo '<div id="dashboarddiv"><iframe id="dashboardiframe" src="'.ZOPIM_DASHBOARD_URL.'i" height=700 width=98% scrolling="no"></iframe></div>      You may also <a href="'.ZOPIM_DASHBOARD_URL.'" target="_newWindow" onClick="javascript:document.getElementById(\'dashboarddiv\').innerHTML=\'\'; ">access the dashboard in a new window</a>.
+		';
+}
+
+// Register the option settings we will be using
+function register_zopim_plugin_settings() {
+
+	// Authentication and codes
+	register_setting( 'zopim-settings-group', 'zopimCode' );
+	register_setting( 'zopim-settings-group', 'zopimUsername' );
+	register_setting( 'zopim-settings-group', 'zopimSalt' );
+	register_setting( 'zopim-settings-group', 'zopimUseSSL' );
+	// General Widget settings
+	register_setting( 'zopim-settings-group', 'zopimGetVisitorInfo' );
+	register_setting( 'zopim-settings-group', 'zopimLang' );
+	// Chat button settings
+	register_setting( 'zopim-settings-group', 'zopimPosition' );
+	register_setting( 'zopim-settings-group', 'zopimHideOnOffline' );
+	register_setting( 'zopim-settings-group', 'zopimBubbleTitle' );
+	register_setting( 'zopim-settings-group', 'zopimBubbleText' );
+	register_setting( 'zopim-settings-group', 'zopimBubbleEnable' );
+	register_setting( 'zopim-settings-group', 'zopimUseBubble' );
+	// Themes / Color
+	register_setting( 'zopim-settings-group', 'zopimColor' );
+	register_setting( 'zopim-settings-group', 'zopimTheme' );
+	// Message Settings
+	register_setting( 'zopim-settings-group', 'zopimGreetings' );
+	register_setting( 'zopim-settings-group', 'zopimUseGreetings' );
+
+	get_option('zopimCode')         == "" && update_option('zopimCode', "zopim");
+	get_option('zopimBubbleTitle')  == "" && update_option('zopimBubbleTitle', "Questions?");
+	get_option('zopimBubbleText')   == "" && update_option('zopimBubbleText', "Click here to chat with us!");
+	get_option('zopimBubbleEnable') == "" && update_option('zopimBubbleEnable', "checked");
+	get_option('zopimUseGreetings') == "" && update_option('zopimUseGreetings', "disabled");
+	get_option('zopimUseBubble')    == "" && update_option('zopimUseBubble', "disabled");
+	get_option('zopimGreetings')    == "" && update_option('zopimGreetings', '{"away":{"window":"If you leave a question or comment, our agents will be notified and will try to attend to you shortly =)","bar":"Click here to chat"},"offline":{"window":"We are offline, but if you leave your message and contact details, we will try to get back to you =)","bar":"Leave a message"},"online":{"window":"Leave a question or comment and our agents will try to attend to you shortly =)","bar":"Click here to chat"}}');
+}
+
+add_action('get_footer', 'zopimme');
+// create custom plugin settings menu
+add_action('admin_menu', 'zopim_create_menu');
+
+function zopim_post_request($url, $_data, $optional_headers = null)
+{
+	if (get_option('zopimUseSSL') != "zopimUseSSL")
+		$url = str_replace("https", "http", $url);
+
+	$args = array('body' => $_data);
+	$response = wp_remote_post( $url, $args );
+	return $response['body'];
+
+}
+
+function zopim_url_get($filename) {
+	$response = wp_remote_get($filename);
+	return $response['body'];
+}
+
+function json_to_array($json) {
+	require_once('JSON.php');
+	$jsonparser = new Services_JSON();
+	return ($jsonparser->decode($json));
+}
+
+function to_json($variable) {
+	require_once('JSON.php');
+	$jsonparser = new Services_JSON();
+	return ($jsonparser->encode($variable));
+}
+
+function getAccountDetails($salt) {
+	$salty = array("salt" => get_option('zopimSalt'));
+	return json_to_array(zopim_post_request(ZOPIM_GETACCOUNTDETAILS_URL, $salty));
+}
+
+?>
